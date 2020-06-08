987dd32f0b661ecd4f5e64f05e140cbc832af739
Kneeco <nicolas.andre.pf@gmail.com>
2020-06-08 08:32:48 +0800

    List rendering, Keys, and CSS Styling.

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Person from './Person'
 function NameList() {
     const persons = [
         {
@@ -12,7 +12,7 @@ function NameList() {
             id: 2,
             name: 'Clark',
             age: 25,
-            skill:'Angular'
+            skill: 'Angular'
         },
         {
             id: 3,
@@ -21,15 +21,18 @@ function NameList() {
             skill: 'Vue'
         }
     ]
-    const personList = persons.map(person => <h2>I am {person.name}. I am {person.age} years old. I know {person.skill}.</h2>)
-   /*  const names = ['Bruce', 'Clark', 'Diana']
-    const nameList = names.map(name => <h2>{name}</h2>) */
+    const personList = persons.map(person => <Person key={person.name} person={person} />)
+
+
+      const names = ['Bruce', 'Clark', 'Diana']
+     const nameList = names.map((name, index) => <h2 key={index}> Index: {index} : {name} </h2>) 
+     
     return (
         <div>
-            {personList}
-            {/* {nameList} */}
+            {/* {personList} */}
+             {nameList} 
         </div>
     );
-}
+} 
 
 export default NameList;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -12,6 +12,7 @@ class Welcome extends Component {
     
     render(){
         const {name, heroName} = this.props
+        //const {state1, state2} = this.state
         return <h1>Welcome {name} a.k.a {heroName}</h1>
     }
 }
